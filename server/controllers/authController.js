dd861c757541d9867245beae136247ad0e7e8115
Bashir Amokomowo <barsheyr@gmail.com>
2025-08-18 13:44:19 +0100

    fix

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -31,6 +31,7 @@ export const login = async (req, res) => {
     httpOnly: true,
     expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
   });
   res.status(StatusCodes.OK).json({ msg: "user logged in" });
 };
